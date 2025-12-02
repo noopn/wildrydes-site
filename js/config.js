b566ea544bc5aab0366cf35bd23330a313802140
Bowen <25141562+noopn@users.noreply.github.com>
2025-12-02 14:39:00 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-west-1_4bZS3Drse', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4j9qjcjstgvctpdoub942mmdfd', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv,
+        userPoolClientId: '38v39976572bcr9pvmkdros6vk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv,
         userPoolClientSecret: '10ngptiqsijugdc2gf89iqhe1406aqs4ajbb2rp751uh1vtpbror',
         region: 'us-west-1' // e.g. us-east-2
     },
@@ -9,5 +9,6 @@ window._config = {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
